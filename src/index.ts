ca1677f852a1041b330711f839d5dee4cc87d379
Artem Ilinykh <devsinglesly@gmail.com>
2020-06-20 22:13:21 +0600

    index define

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @package
+ * @author Artem Ilinykh devsinglesly@gmail.com
+ * @file index
+ */
+export * from "./decorator/InjectEventEmitter";
+export * from "./decorator/Listener";
+export * from "./constant/constant";
+export * from "./EventEmitter";
+export * from "./EventEmitterInterface";
+export * from "./ListenerInterface";
+export * from "./EventInterface";
+export * from "./NestAsyncEventModule";
